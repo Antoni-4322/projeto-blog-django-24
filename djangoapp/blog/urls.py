7a57e892296f10a4dc5eff8636965439f139b41e
Antonio Sousa <antoniosousaferreirq@gmail.com>
2024-02-03 10:08:53 -0300

    pagina admin e post do blog desenvolvidas

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/<slug:slug>/', post, name='post'),
+    path('page', page, name='page'),
 ]
 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from blog.models import Post
+
+PER_PAGE = 9
 
 def index(request):
+    posts = Post.objects.get_published()
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {
+            'page_obj':page_obj
+        }
+    )
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html'
+    )
+
+def post(request, slug):
+    post = (
+            Post.objects.get_published()
+            .filter(slug=slug)
+        )
+    return render(
+        request,
+        'blog/pages/post.html'
     )
